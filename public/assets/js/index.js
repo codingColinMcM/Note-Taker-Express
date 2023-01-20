71388f1fa3cd25e43d07220d72dffbcab919cc90
Colin McMurtray <75285902+cmcmurtr@users.noreply.github.com>
2023-01-19 20:17:38 -0500

    heroku error

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,7 +4,7 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-if (window.location.href == '/notes.html') {
+if (window.location.href == 'https://fierce-spire-62517.herokuapp.com/notes.html') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
@@ -166,7 +166,7 @@ const renderNoteList = async () => {
     noteListItems.push(li);
   });
 
-  if (window.location.href == '/notes.html') {
+  if (window.location.href == 'https://fierce-spire-62517.herokuapp.com/notes.html') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
 };
@@ -174,7 +174,7 @@ const renderNoteList = async () => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.href == '/notes.html') {
+if (window.location.href == 'https://fierce-spire-62517.herokuapp.com/notes.html') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
